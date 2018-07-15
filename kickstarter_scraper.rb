cccc242c0ca88e8863c8f6aeb578c06da17a6241
Joana Palero <joana.palero24@gmail.com>
2018-07-15 12:14:27 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,11 @@ def create_project_hash
   projects.each do |project|
     title = project.css(h2.bbcard_name strong a").text.to_sym
     project_hash[title] = {
-      
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("ul.project-meta li a span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
+  end
+    project_hash
 end
