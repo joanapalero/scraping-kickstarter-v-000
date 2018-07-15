3fe204ec1b504d073ca6d775f6bce4faf291bd28
Joana Palero <joana.palero24@gmail.com>
2018-07-15 12:14:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,4 +7,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   project_hash{}
+  projects = kickstarter.css("li.project.grid_4")
+  projects.each do |project|
+    title = project.css(h2.bbcard_name strong a").text.to_sym
+    project_hash[title] = {
+      
+    }
 end
